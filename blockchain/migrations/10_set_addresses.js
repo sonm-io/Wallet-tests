9f10acf0b388cf74b4dbcdfe1d2c93b061f5e99d
Aleksander Efimov <aleksander_efimov@sonm.io>
2018-07-18 13:22:14 +0300

    fix token addresses #2

--- blockchain/migrations/10_set_addresses.js
+++ blockchain/migrations/10_set_addresses.js
@@ -2,9 +2,7 @@ let SNM = artifacts.require('./SNM.sol');
 let Market = artifacts.require('./Market.sol');
 let Blacklist = artifacts.require('./Blacklist.sol');
 let OracleUSD = artifacts.require('./OracleUSD.sol');
-let TestnetFaucet = artifacts.require('./TestnetFaucet.sol');
 let SimpleGatekeeperWithLimit = artifacts.require('./SimpleGatekeeperWithLimit.sol');
-let SimpleGatekeeperWithLimitLive = artifacts.require('./SimpleGatekeeperWithLimitLive.sol');
 let ProfileRegistry = artifacts.require('./ProfileRegistry.sol');
 let AddressHashMap = artifacts.require('./AddressHashMap.sol');
 const fs = require("fs");
@@ -29,7 +27,7 @@ module.exports = function (deployer, network) {
             addrHashmap.write('profileRegistryAddress', ProfileRegistry.address, {gasPrice: 0});
             addrHashmap.write('oracleUsdAddress', OracleUSD.address, {gasPrice: 0});
             addrHashmap.write('gatekeeperSidechainAddress', SimpleGatekeeperWithLimit.address, {gasPrice: 0});
-            addrHashmap.write('testnetFaucetAddress', TestnetFaucet.address, {gasPrice: 0});
+            addrHashmap.write('testnetFaucetAddress', '0x8e95a66e0a038e2325185b10eaae8854c9896835', {gasPrice: 0});
             await addrHashmap.write('oracleMultiSigAddress', 0x0, {gasPrice: 0});
             dump(AddressHashMap.address);
         } else if (network === 'livenet') {
@@ -44,7 +42,7 @@ module.exports = function (deployer, network) {
             addrHashmap.write('profileRegistryAddress', ProfileRegistry.address);
             addrHashmap.write('oracleUsdAddress', OracleUSD.address);
             addrHashmap.write('gatekeeperSidechainAddress', SimpleGatekeeperWithLimit.address);
-            addrHashmap.write('testnetFaucetAddress', 0x0);
+            addrHashmap.write('testnetFaucetAddress', '0x8e95a66e0a038e2325185b10eaae8854c9896835');
             await addrHashmap.write('oracleMultiSigAddress', 0x0);
         }
     });
